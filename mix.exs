a66c26ae2f680793d8ca46855ee8a454ceb9630d
Will Raxworthy <me@willrax.com>
2015-12-27 20:18:51 +0000

    bump to latest version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Marvin.Mixfile do
 
   def project do
     [app: :marvin,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
